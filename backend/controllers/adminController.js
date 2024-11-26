8a03e8fe6c2a60ff5771b3f27d9b737885dac644
Ismaeel Owolabi <owolabismaeel@gmail.com>
2024-11-26 20:57:56 +0100

    frontend

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -2,6 +2,7 @@ import validator from "validator";
 import bcrypt from "bcryptjs";
 import { v2 as cloudinary } from "cloudinary";
 import doctorModel from "../models/doctorModel";
+import jwt from "jsonwebtoken";
 
 //API for adding Doctor
 async function addDoctor() {
@@ -95,8 +96,21 @@ async function addDoctor() {
 //  API for Admin login
 const loginAdmin = async (req, res) => {
   try {
-    const { email, password } = re.body;
-  } catch (error) {}
+    const { email, password } = req.body;
+
+    if (
+      email === process.env.ADMIN_EMAIL &&
+      password === process.env.ADMIN_PASSWORD
+    ) {
+      const token = jwt.sign(email + password, process.env.JWT_SECRET);
+      res.json({ success: true, token });
+    } else {
+      res.json({ success: false, message: "Invalid credentials" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
 };
 
 export { addDoctor, loginAdmin };
